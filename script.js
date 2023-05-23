c749e0b1cc94f3d8ce0833b31e8ecfa587197c9d
Del1rius <timothy.aidan.barry@gmail.com>
2023-05-23 14:48:39 +0200

    js changes again

--- script.js
+++ script.js
@@ -3,19 +3,21 @@ let weight, height, bmiCalc;
 function calculateBMI(weight, height, bmiCalc) {
   weight = parseFloat(document.getElementById("weight").value);
   height = parseFloat(document.getElementById("height").value);
-  bmiCalc = (weight / height ** 2)*10000;
+//   bmiCalc = (weight / height ** 2)*10000;
+bmiCalc = (weight / Math.pow(height/100, 2)).toFixed(1)
 
   if (bmiCalc <= 18) {
-    document.getElementById("bmi").value = bmiCalc.toFixed(2) + "Underweight🦴";
+    document.getElementById("bmi").value = bmiCalc + " Underweight🦴";
 
   } else if (bmiCalc > 18.4 && bmiCalc <= 24.9) {
-    document.getElementById("bmi").value = bmiCalc.toFixed(2) + "Normal💫";
+    document.getElementById("bmi").value = bmiCalc + " Normal💫";
 
    } else if (bmiCalc > 25 && bmiCalc <= 39.9) {
-    document.getElementById("bmi").value = bmiCalc.toFixed(2) + "Overweight🤨";
+    document.getElementById("bmi").value = bmiCalc + " Overweight🤨";
 
    } else if (bmiCalc >= 40 ) {
-    document.getElementById("bmi").value = bmiCalc.toFixed(2) + " Obese♿";
+    document.getElementById("bmi").value = bmiCalc
+     + " Obese♿";
 
    } else {
    document.getElementById("bmi").value = "";
