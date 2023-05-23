41d4b243509ae46fd8ecae0cd74f67746778f24d
Del1rius <timothy.aidan.barry@gmail.com>
2023-05-23 12:10:31 +0200

    js changes

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ function calculateBMI(weight, height, bmiCalc) {
   bmiCalc = (weight / height ** 2)*10000;
 
   if (bmiCalc <= 18) {
-    document.getElementById("bmi").value = "Underweight🦴";
+    document.getElementById("bmi").value = bmiCalc.toFixed(2) + "Underweight🦴";
 
   } else if (bmiCalc > 18.4 && bmiCalc <= 24.9) {
-    document.getElementById("bmi").value = "Normal💫";
+    document.getElementById("bmi").value = bmiCalc.toFixed(2) + "Normal💫";
 
    } else if (bmiCalc > 25 && bmiCalc <= 39.9) {
-    document.getElementById("bmi").value = "Overweight🤨";
+    document.getElementById("bmi").value = bmiCalc.toFixed(2) + "Overweight🤨";
 
    } else if (bmiCalc >= 40 ) {
-    document.getElementById("bmi").value = "Obese♿";
+    document.getElementById("bmi").value = bmiCalc.toFixed(2) + " Obese♿";
 
    } else {
    document.getElementById("bmi").value = "";
