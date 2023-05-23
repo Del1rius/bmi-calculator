1bc2ec09a8737f0e42b06af5a6ba3916571479bb
Del1rius <timothy.aidan.barry@gmail.com>
2023-05-23 12:04:52 +0200

    Js complete

--- script.js
+++ script.js
@@ -4,16 +4,21 @@ function calculateBMI(weight, height, bmiCalc) {
   weight = parseFloat(document.getElementById("weight").value);
   height = parseFloat(document.getElementById("height").value);
   bmiCalc = (weight / height ** 2)*10000;
-//   document.getElementById("bmi").value = bmiCalc;
 
   if (bmiCalc <= 18) {
     document.getElementById("bmi").value = "Underweight🦴";
 
   } else if (bmiCalc > 18.4 && bmiCalc <= 24.9) {
-        document.getElementById("bmi").value = "Normal💫";
+    document.getElementById("bmi").value = "Normal💫";
 
    } else if (bmiCalc > 25 && bmiCalc <= 39.9) {
-        document.getElementById("bmi").value = "Overweight🤨";
+    document.getElementById("bmi").value = "Overweight🤨";
 
-   } 
-}
+   } else if (bmiCalc >= 40 ) {
+    document.getElementById("bmi").value = "Obese♿";
+
+   } else {
+   document.getElementById("bmi").value = "";
+    }
+
+};
